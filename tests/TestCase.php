c38b1745c61d9baea5e5916adac6b3fed6b8a8bd
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-01-19 23:20:19 +0330

    disable exception handler has been added

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Exception;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function disableExceptionHandler()
+    {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(Exception $e) {}
+            public function render($request, Exception $e) {
+                throw $e;
+            }
+        });
+    }
+
 }
