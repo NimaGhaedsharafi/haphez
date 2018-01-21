1c5300dd03246d30684818d70179d9e3956bb54c
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2018-01-21 23:32:32 +0330

    secret model has been defined

---
+++ app/Secret.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Secret
+ * @package App
+ */
+class Secret extends Model
+{
+    use SoftDeletes;
+}
