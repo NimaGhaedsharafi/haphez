c81c683379aa91039d52af1d3d813cd8ee0e636b
Nima Ghaedsharafi <>
2018-07-12 18:52:36 +0430

    instaniate the Database class

--- tests/Unit/DatabaseTest.php
+++ tests/Unit/DatabaseTest.php
@@ -9,11 +9,13 @@
 namespace Tests\Unit;
 
 
+use App\Services\MI6\DatabaseSecret;
 use Tests\TestCase;
 
 class DatabaseTest extends TestCase
 {
     public function test_create_a_secret()
     {
+        $service = new DatabaseSecret();
     }
 }
