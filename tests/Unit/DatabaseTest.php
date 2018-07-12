084e2e40029837b89b4031d91ebe08eb2f53bddb
Nima Ghaedsharafi <>
2018-07-12 18:51:14 +0430

    let's do it TDD

---
+++ tests/Unit/DatabaseTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nghaedsharafi
+ * Date: 7/12/18
+ * Time: 6:50 PM
+ */
+
+namespace Tests\Unit;
+
+
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+
+}
