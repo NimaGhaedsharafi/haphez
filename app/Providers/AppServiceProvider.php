cbdbb766d1f68bfee5f68e49724b13e50c46713f
Nima Ghaedsharafi <>
2018-07-12 19:49:36 +0430

    bind SecretService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\MI6\DatabaseSecret;
+use App\Services\MI6\SecretService;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(SecretService::class, DatabaseSecret::class);
     }
 }
