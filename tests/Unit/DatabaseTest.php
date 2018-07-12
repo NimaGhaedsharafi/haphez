f55a06b9e7f98a366b0e80458f32503dfc3393e2
Nima Ghaedsharafi <>
2018-07-12 18:54:51 +0430

    add type hint on service variable

--- tests/Unit/DatabaseTest.php
+++ tests/Unit/DatabaseTest.php
@@ -10,12 +10,16 @@ namespace Tests\Unit;
 
 
 use App\Services\MI6\DatabaseSecret;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class DatabaseTest extends TestCase
 {
     public function test_create_a_secret()
     {
+        /** @var DatabaseSecret $service */
         $service = new DatabaseSecret();
+        $publicId = $service->store('my secret', Carbon::tomorrow());
+
     }
 }
