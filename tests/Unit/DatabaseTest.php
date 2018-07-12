230fe3c8a252ce60116dc4217e74946140398fcf
Nima Ghaedsharafi <>
2018-07-12 19:09:41 +0430

    define get secret test

--- tests/Unit/DatabaseTest.php
+++ tests/Unit/DatabaseTest.php
@@ -28,4 +28,15 @@ class DatabaseTest extends TestCase
         $this->assertNotEmpty($publicId);
         $this->assertDatabaseHas((new Secret())->getTable(), ['public_id' => $publicId]);
     }
+
+    public function test_get_a_secret()
+    {
+        /** @var DatabaseSecret $service */
+        $service = new DatabaseSecret();
+        $publicId = $service->store('my secret', Carbon::tomorrow());
+
+        $this->assertNotEmpty($publicId);
+
+        $this->assertEquals('my secret', $service->get($publicId));
+    }
 }
