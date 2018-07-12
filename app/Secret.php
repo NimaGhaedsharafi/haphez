39eab8c30155266c9b5737ba77c9368433bd3981
Nima Ghaedsharafi <>
2018-07-12 19:44:52 +0430

    add property notation to Secret model

--- app/Secret.php
+++ app/Secret.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * Class Secret
  * @package App
+ * @property int $id
+ * @property string $message
+ * @property string $public_id
+ * @property \Carbon\Carbon $expires_in
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $deleted_at
  */
 class Secret extends Model
 {
