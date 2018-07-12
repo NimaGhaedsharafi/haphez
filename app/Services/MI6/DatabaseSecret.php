1e72ac48daf093333a29491650925304adc11ed5
Nima Ghaedsharafi <>
2018-07-12 18:48:22 +0430

    add stubs

--- app/Services/MI6/DatabaseSecret.php
+++ app/Services/MI6/DatabaseSecret.php
@@ -9,7 +9,27 @@
 namespace App\Services\MI6;
 
 
+use Carbon\Carbon;
+
 class DatabaseSecret implements SecretService
 {
 
+    /**
+     * @param string $message
+     * @param Carbon $expiresIn
+     * @return string PublicId
+     */
+    public function store(string $message, Carbon $expiresIn)
+    {
+        // TODO: Implement store() method.
+    }
+
+    /**
+     * @param string $publicId
+     * @return string Message
+     */
+    public function get(string $publicId)
+    {
+        // TODO: Implement get() method.
+    }
 }
