635c1946804712870447a2f442ee9cc7a57391fc
Nima Ghaedsharafi <>
2018-07-12 18:45:02 +0430

    add PhpDoc

--- app/Services/MI6/SecretService.php
+++ app/Services/MI6/SecretService.php
@@ -12,5 +12,10 @@ use Carbon\Carbon;
 
 interface SecretService
 {
+    /**
+     * @param string $message
+     * @param Carbon $expiresIn
+     * @return string PublicId
+     */
     public function store(string $message, Carbon $expiresIn);
 }
